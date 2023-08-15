d0cc0f061cd4f034bf52915527dc50cd092b0df1
emphasize <swen_g@t-online.de>
2023-08-15 15:08:26 +0200

    add versioning tools

--- setup.py
+++ setup.py
@@ -3,6 +3,31 @@ import os
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'quebra_frases', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
+
 def required(requirements_file):
     """ Read requirements file and remove comments and empty lines. """
     with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
@@ -13,14 +38,38 @@ def required(requirements_file):
         return [pkg for pkg in requirements
                 if pkg.strip() and not pkg.startswith("#")]
 
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
 setup(
     name='quebra_frases',
-    version='0.3.7',
-    packages=['quebra_frases'],
-    url='https://github.com/OpenJarbas/quebra_frases',
-    install_requires=required("requirements.txt"),
-    license='apache-2.0',
+    version=get_version(),
+    description='Tokenizer tool to chunks strings into byte sized pieces',
+    url='https://github.com/OpenVoiceOS/quebra_frases',
     author='jarbasAi',
     author_email='jarbasai@mailfence.com',
-    description='quebra_frases chunks strings into byte sized pieces'
+    license='Apache-2.0',
+    packages=['quebra_frases'],
+    install_requires=required("requirements.txt"),
+    package_data={'': package_files('quebra_frases')},
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Text Processing :: Linguistic',
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ]
 )
